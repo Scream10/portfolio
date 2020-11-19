4d76d13cf8aec55fc4b05a37bc88708c1bca3405
Nicolas Diot <nicolasdiot10@gmail.com>
2020-11-19 16:55:40 +0100

    add links and change contact page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,13 @@ function Footer () {
                 <div className="footer__info">
                     <div className="footer__contact">
                         <h4 className="header-underline">Contact</h4>
-                        <p>nicolasdiot10@gmail.com</p>
-                        <p>+33 (0)6 35 39 08 49</p>
+                        <p><a href="mailto:nicolasdiot10@gmail.com?subject=Nicolas Diot Portfolio - Développeur Web">nicolasdiot10@gmail.com</a></p>
+                        <p><a href="tel:0635390849">+33 (0)6 35 39 08 49</a></p>
                     </div>
                     <div className="footer__social">
                         <h4 className="header-underline header-underline--social">Social</h4>
-                        <i className="fab fa-github-square fa-2x"></i>
-                        <i className="fab fa-linkedin fa-2x"></i>
+                        <a href="https://github.com/Scream10" target="_blank" rel="noreferrer"><i className="fab fa-github-square fa-2x"></i></a>
+                        <a href="https://www.linkedin.com/in/nicolas-diot-96b0b313a/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
                     </div>
                 </div>
             </div>
